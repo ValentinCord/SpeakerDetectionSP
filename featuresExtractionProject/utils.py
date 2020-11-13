1ef6318cfd59a068c31499ed25f41094efbdffa5
ValentinCord <64888334+ValentinCord@users.noreply.github.com>
2020-11-13 17:57:15 +0100

    Update utils.py

--- featuresExtractionProject/utils.py
+++ featuresExtractionProject/utils.py
@@ -2,52 +2,43 @@ import numpy as np
 from scipy.io.wavfile import read
 import random
 import matplotlib.pyplot as plt
+import scipy.signal as sgl
+import math
 
-signal = [2, 3, 5, 9, 18, 62, 53, 1, 23, 5, 4, 6, 5, 66, 45, 100, -100]
-sig2 = [1, 1, 0, 0, 1, 1, 0, 0, 1, 1]
-
-
+#============Pre-Processing============
 def norm(signal):
-    #Recherche de l'échantillon max et min
-    maximum = max(signal)
-    minimum = min(signal)
-    
-    #Cas trivial : signal nul
-    if (maximum == minimum == 0):
-        normsignal = signal
-    
-    #Calcul du signal normalisé
-    else:
-        if (maximum > abs(minimum)):
-            normsignal = [float(i) / max(signal) for i in signal]
-
-        else:
-            normsignal = [float(i) / -min(signal) for i in signal]
-
+    array = np.array(signal)
+    #Chercher la valeur maximale du signal en valeur absolue
+    absolute = np.abs(array)
+    maxima = np.max(absolute)
+    #Diviser tout les échantillons par la valeur maximale
+    normsignal = array/maxima
+    normsignal = normsignal.tolist()
     return normsignal
 
-
-def split(signal, fe, Twidth, Tstep):  # fe = nbr of point took every second : 1s -> fe samples <=> Nsamples = duration * fe
-
+def split(signal, fe, Twidth, Tstep):
     #Tests des triviaux
     if (Twidth == Tstep == 0):
         print("la fenetre est de largeur 0 et le saut est de 0")
-        return
+        return 0
     else:
         if (Twidth == 0):
             print("la fenetre est de largeur 0")
-            return
+            return 0
         #Overlap condition
         else:
             if (Twidth > Tstep):
-                print("La condition de non-recouvrement est faussse")
-                return
-
+                print("La condition de non-recouvrement n'est pas vérifiée")
+                return 0
     if (fe == 0):
-        print("la fréquence échantillon est nulle")
-        return
+        print("la fréquence d'échantillonage est nulle")
+        return 0
+    if (signal == 0):
+        print("le signal est nul")
+        return 0
 
     #Calcule de Nwidth & Nstep (passade du domaine temporel au domaine discret)
+    fe = int(fe/1000)
     Nwidth = Twidth * fe  #Nombre de points par frame
     Nstep = Tstep * fe  #Nombre de points par step
 
@@ -82,26 +73,46 @@ def split(signal, fe, Twidth, Tstep):  # fe = nbr of point took every second : 1
 
     return splitlist
 
-
-def energy(signal):
-    energy = 0 #Déclaration à 0 (valeur neutre de la somme)
-    for value in signal:
-        energy += value ** 2
-    return energy
-
-
+#===============Files==================
 def randomfichier():
+    
     #Génération d'un nombre de 001-593 (différent de 1-593)
-    unite = random.randint(0, 9)
     centaine = random.randint(0, 5)
     if (centaine == 5):
         dizaine = random.randint(0, 3)
     else:
         dizaine = random.randint(0, 9)
+    if (centaine == 0 and dizaine == 0):
+        unite = random.randint(1, 9)
+    else:
+        unite = random.randint(0, 9)
     #Concaténation des varaibles pour générer le nom correct du fichier
-    nomfichier = "arctic_b0%i%i%i.wav" % (centaine, dizaine, unite)
+    nomfichier = "arctic_a0%i%i%i.wav" % (centaine, dizaine, unite)
     return nomfichier
 
+def readFiles(choix):
+    
+    if (choix == 'male' or choix == 'female'):
+        if (choix == 'male'):
+            choix = '../cmu_us_bdl_arctic/wav/%s'
+        elif (choix == 'female'):
+            choix = '../cmu_us_slt_arctic/wav/%s'
+        
+        utterance = read(choix % (randomfichier()))
+        fe = utterance[0]
+        sig = utterance[1]
+        signal = sig.tolist()        
+    else : 
+        print('Choisissez entre "male" ou "female"')
+        
+    return signal, fe
+
+#===============Features===============
+def energy(signal):
+    energy = 0 #Déclaration à 0 (valeur neutre de la somme)
+    for value in signal:
+        energy += value ** 2
+    return energy
 
 def frameEnergy(splitlist):
     #Liste de langueur égale au nombre de frames
@@ -112,36 +123,172 @@ def frameEnergy(splitlist):
 
     return B
 
+def xcorr(x, y = None, scale='none', maxlag = None):
 
-# sign= [1, 1, 0, 0, 1, 1, 0, 0, 1, 1]
-# splt = split(sign, 1, 2, 4)
-# print(splt)
+    """compute correlation of x and y.
+    If y not given compute autocorrelation.
+    
+    Arguments:
+        x {np.ndarray} -- signal
+        y {np.ndarray} -- signal
+    
+    Keyword Arguments:
+        scale {str} -- can be either "none", "biased" or "unbiased" (default: {'none'})
+        maxlag {str} -- maximum lag to be returned. This should be <= round(y.size+x.size-1)/2 (default: {'none'})
+    Returns:
+        [np.ndarry] -- corresponding lags
+        [np.ndarray] -- resulting correlation signal
+    """
+    # If y is None ccmpute autocorrelation
+    if y is None:
+        y = x
+    # Pad shorter array if signals are different lengths
+    else:
+        if x.size > y.size:
+            pad_amount = x.size - y.size
+            y = np.append(y, np.repeat(0, pad_amount))
+        elif y.size > x.size:
+            pad_amount = y.size - x.size
+            x = np.append(x, np.repeat(0, pad_amount))
+    if maxlag is None:
+        maxlag = (x.size+y.size-1)/2
+    if maxlag>round((y.size+x.size-1)/2):
+        raise ValueError("maxlag should be <= round(y.size+x.size-1)/2")
+    corr = np.correlate(x, y, mode='full')  # scale = 'none'
+    lags = np.arange(-maxlag, maxlag + 1)
+    # lags = np.arange(-x.size, x.size-1)
+    corr = corr[(x.size-1-maxlag):(x.size + maxlag)]
+    if scale == 'biased':
+        corr = corr / x.size
+    elif scale == 'unbiased':
+        corr /= (x.size - abs(lags))
+    elif scale == 'coeff':
+        corr /= np.sqrt(np.dot(x, x) * np.dot(y, y))
+    # lags = lags[int(round(len(lags)/2)-maxlag+1) : int(round(len(lags)/2)+maxlag-1)]
+    # corr = corr[int(round(len(corr)/2)-maxlag+1) : int(round(len(corr)/2)+maxlag-1)]
+    return lags, corr
+  
+def autocorr(signal, fe, Enerseuil):
+    
+    listpitch = [] # on initialise notre liste 
+    energy = frameEnergy(signal)
+    
+    for i in range(len(signal)): # demarre une boucl for afin de parcourir toutes les frames du signal
+        
+       if(energy[i-1] > Enerseuil): # On ne prends que les valeurs avec un certains seuil d energie 
+           arrsignal= np.array(signal[i-1]) # transforme notre signal en array list afin de faciiliter les calculs 
+           lags, corr = xcorr(arrsignal, maxlag = int(fe/50))# Utilise la fct corr pour etudier les correlations
+           #plt.plot(corr)
+           maxima, value = sgl.find_peaks(corr, 0) # on cherche tous les maxima
+           Hvalue = value['peak_heights']# Lorsqu on lit les "values", cela nous affiche {'peak_heights':array([... ])} et on veut juste array
+           # "maxima" correspond a la position des sommets sur l axe des x et "value" correspond aux hauteurs de ceux-ci 
+           maxima = maxima.tolist() # On transforme "maxima" en array list
+         
+           if (len(maxima) > 1):
+                Hvalue.sort() # on trie par ordre croissant les hauteurs 
+                temp = np.abs(maxima[len(maxima)-1] - maxima[len(maxima)-2])# on calcul la distance, sur l axe des x, des 2 hauteurs les plus significatives 
 
+                ffond = fe// temp# on calcul la frequence fondamentale 
+                listpitch.append(ffond) # introduit la donnee dans la liste
+           
+       else : 
+          ffond = 0 # si les valeurs sont en dessous du seuil d energie, on considere la valeurs comme nulle 
+          listpitch.append(ffond)
+            
+    Vpitch = np.array(listpitch)# transforme la liste en array list pour faciliter les calculs
+    Fpitch = abs(np.mean(Vpitch))# calcul la moyenne de l array liste  
 
-utterance = read("../cmu_us_ksp_arctic/wav/%s" % (randomfichier()))
-fetot = utterance[0]
-fe = int(fetot / 1000)
-print(fe)
-print(utterance)
-sig = utterance[1]
-signal = sig.tolist()
-print(signal)
-normsignal = norm(signal)
-print(normsignal)
-Twidth = 10
-Tstep = 100
-splitsignal = split(normsignal, fe, Twidth, Tstep)
-print(splitsignal)
+    return listpitch
+      
+def formant( signal, fe):
+       
+    for i in range(len(signal)):
+        framefilter = np.array(sgl.lfilter([1.,-0.67],1,signal[i-1]))
+        
+        N = len(framefilter)
+        framehamming = framefilter*sgl.hamming(N)
+        
+        # get LPC
+        A = lpc_ref(framehamming, 8)
 
-EnergySignal = frameEnergy(splitsignal)
+        # get roots 
+        rts = np.roots(A)
+        
+        #angz = np.array(np.angle(rts)) ou 
+        angz = np.arctan2(np.imag(rts), np.real(rts))
 
-print(EnergySignal)
+        frqs = np.array(np.unique(abs(angz * (fe / (2 * np.pi)))))
+        
+        frqs.tolist()
+        frqs.sort()
+        
+    return frqs
 
-plt.plot(splitsignal, color='black')
-plt.plot(EnergySignal, '+', color='red')
-plt.show()
+#========Discriminatory rules=========
+def ruleEnergy(n):
+    Twidth = 100
+    Tstep = 100
+    for i in range (n):        
+        signal, fe = readFiles('male')
+        normsignal = norm(signal)
+        splitsignal = split(normsignal, fe, Twidth, Tstep)
+        energysignal = frameEnergy(splitsignal)
+        plt.plot(energysignal, '+', color = 'blue')
+        
+        signal, fe = readFiles('female')
+        normsignal = norm(signal)
+        splitsignal = split(normsignal, fe, Twidth, Tstep)
+        energysignal = frameEnergy(splitsignal)
+        plt.plot(energysignal, '+', color = 'pink')
+    plt.show()
+        
+def rulePitch(n):
+    Twidth = 100
+    Tstep = 100
+    for i in range(n):
+        signal, fe = readFiles('male')
+        normsignal = norm(signal)
+        splitsignal = split(normsignal, fe, Twidth, Tstep)
+        listpitch = autocorr(splitsignal, fe, 50)
+        plt.plot(listpitch, '+', color = 'blue')
+        
+        signal, fe = readFiles('female')
+        normsignal = norm(signal)
+        splitsignal = split(normsignal, fe, Twidth, Tstep)
+        listpitch = autocorr(splitsignal, fe, 50)
+        plt.plot(listpitch, '+', color = 'pink')
+    plt.show()
 
-plt.plot(splitsignal, color='black')
-plt.show()
+def ruleFormants():
+    
+    return 0
 
+#==========Rule-based Systeme=========
+def ruleBasedSystem(n):
+    Twidth = 100
+    Tstep = 100
+    score = 0
+    for i in range(n):
+        if (random.randint(0,100)%2 == 0):
+            typefichier = 'female'
+        else:
+            typefichier = 'male'
+        signal ,fe = readFiles(typefichier)
+        normsignal = norm(signal)
+        splitsignal = split(normsignal, fe, Twidth, Tstep)
+        energysignal = frameEnergy(splitsignal)
+        #pitch
+        #formants
+        array = np.array(energysignal)
+        if (max(array) < 150):
+            if(typefichier == 'male'):
+                score += 1
+        else:
+            if(typefichier == 'female'):
+                score +=1
+            
+        #return male += 1 or female += 1
+    accuracy = score/n
+    return accuracy
 
+print(ruleBasedSystem(50))
